4c4a06030120224871cde8ad6b057f827fba7376
yash jayawant <131981213+Jayawantyash@users.noreply.github.com>
2025-01-10 04:15:51 +0530

    Add files via upload
    gemii contain the info for image to  text

---
+++ gemaa.py
@@ -0,0 +1,45 @@
+import streamlit as st #ignore
+import os
+import textwrap
+
+
+from IPython.display import display
+from IPython.display import Markdown
+#from altair.vegalite.v4.api import Chart
+
+def to_markdown(text):
+  text = text.replace('•', '  *')
+  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+
+os.environ['GEMINI_API_KEY'] = 'AIzaSyDgv04wJGAnVUujAkfoe7o_xdZ2aT32zdg'
+
+
+import google.generativeai as genai
+genai.configure(api_key=os.environ['GEMINI_API_KEY'])
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(question):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content('what is the speed of light')
+    return response.text
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("yashBOT Application")
+
+input=st.text_input("Input: ",key="input")
+
+
+submit=st.button("I can help on your question")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input)
+    st.subheader("The Response is")
+    st.write(response)
---
+++ gemii.py
@@ -0,0 +1,47 @@
+
+import streamlit as st
+import os
+import textwrap
+from PIL import Image
+
+
+
+os.environ['GEMINI_API_KEY'] = 'AIzaSyDgv04wJGAnVUujAkfoe7o_xdZ2aT32zdg'
+
+
+import google.generativeai as genai
+genai.configure(api_key=os.environ['GEMINI_API_KEY'])
+
+
+## Function to load OpenAI model and get respones
+
+def get_gemini_response(input,image):
+    model = genai.GenerativeModel('models/gemini-2.0-flash-thinking-exp-1219')
+    if input!="":
+       response = model.generate_content([input,image])
+    else:
+       response = model.generate_content(image)
+    return response.text
+
+##initialize our streamlit app
+
+st.set_page_config(page_title=" Image CREATION ")
+
+st.header("YASH AI IMAGE APP")
+input=st.text_input("Input Prompt: ",key="input")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image=""   
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image.", use_column_width=True)
+
+
+submit=st.button("Explain me about the image")
+
+## If ask button is clicked
+
+if submit:
+    
+    response=get_gemini_response(input,image)
+    st.subheader("The Response is")
+    st.write(response)
